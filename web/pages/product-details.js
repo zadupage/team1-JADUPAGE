9cb77e6744aba6fdc4cc15b8379c6f70ee9f31fa
YoungjaeJang7 <jyj.firstid@gmail.com>
2025-12-31 17:34:05 +0900

    feat: header.html, footer,html / fix : html-icon, css-icon

--- web/pages/product-details.js
+++ web/pages/product-details.js
@@ -1,3 +1,9 @@
+fetch('http://localhost:3000/products')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+
 const tabbtn = document.getElementById('tab-btn');
 const tabReview = document.getElementById('tab-review');
 const tabQna = document.getElementById('tab-qna');
