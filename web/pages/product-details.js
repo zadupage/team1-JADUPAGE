a046ada8049db29857907281900b52e1d696c61e
YoungjaeJang7 <jyj.firstid@gmail.com>
2026-01-02 13:38:23 +0900

    feat: modal추가(html, css, js)

--- web/pages/product-details.js
+++ web/pages/product-details.js
@@ -1,9 +1,4 @@
-fetch('http://localhost:3000/products')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-  });
-
+// tab 버튼 //
 const tabbtn = document.getElementById('tab-btn');
 const tabReview = document.getElementById('tab-review');
 const tabQna = document.getElementById('tab-qna');
@@ -16,4 +11,44 @@ tabs.forEach(tab => {
     tabs.forEach(t => t.classList.remove('active'));
     tab.classList.add('active');
   });
+});
+
+// 로그인 모달 //
+
+const buyBtn = document.querySelector('.buy');
+const cartBtn = document.querySelector('.장바구니');
+
+const modal = document.getElementById('loginModal');
+const modalNo = document.getElementById('modalNo');
+const modalYes = document.getElementById('modalYes');
+const overlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.modal-close');
+
+const isLogin = false; // 임시
+
+function openModal() {
+  modal.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+}
+
+function handleRequireLogin(e) {
+  e.preventDefault();
+  if (!isLogin) {
+    openModal();
+    return;
+  }
+}
+
+buyBtn.addEventListener('click', handleRequireLogin);
+cartBtn.addEventListener('click', handleRequireLogin);
+
+modalNo.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+
+modalYes.addEventListener('click', () => {
+  window.location.href = '/login.html';
 });
